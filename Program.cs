b76548eaa27766ab671d90c0463e9243a80b2c59
Кодиров Султон <sultonkodiro109@gmail.com>
2020-05-05 00:38:00 +0500

    Старый проект. Убрано локер и уменьшено время выпадение цепочков

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Task_2
+{
+    static class Program
+    {
+
+        static void Main()
+        {
+            Console.SetWindowSize(70, 42);
+ 
+            Matrix instance;
+ 
+            for (int i = 0; i < 20; i++)
+            {
+                instance = new Matrix(i * 3, true);
+                new Thread(instance.Shower).Start();
+            }
+           
+        }
+
+    }
+}
